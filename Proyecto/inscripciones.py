0c14c11992b1f736298729adcb1f4156ef796918
Michel <valerie01liz01@gmail.com>
2024-04-30 11:55:55 -0500

    proyecto

--- Proyecto/inscripciones.py
+++ Proyecto/inscripciones.py
@@ -3,14 +3,16 @@
 
 import json
 from os import system
-import jpathpy
 
-
+Sputnik=[]
 
 with open('academico.json','r') as openfile:
     miJSON= json.load(openfile)
 
-
+for i in range (len(miJSON)):
+        if (miJSON[i]["salon"]=="Sputnik"):
+            Sputnik.append(miJSON[i])
+print(Sputnik)
 
 with open('trainer.json','r') as openfile:
     trai= json.load(openfile)
@@ -70,24 +72,57 @@ if opcion == "Coordinador":
             
     usuario = int(input("Número de Identificación: "))
     print("")
-    password = input("Password: ")
+    Contraseña = input("Contraseña: ")
     print("")
 
     for i in coor:
         for x in i["coordinador"]:
+
             if x["id"] == usuario:
-                if "contrasena" in x and x["contrasena"] == password:
+
+                if "contrasena" in x and x["contrasena"] == Contraseña:
                     print("Ingreso exitoso Coordinador", x["nombre"])
                     print("")
 
-                    respuesta = input("""                          Elije una opción       
+                    respuestaUsuario = int(input("""                          Elije una opción       
                           
                           1. Registrar notas
                           2. Crear nuevas rutas
                           3. Módulo de matrículas
                           4. Consultar campers en alto riesgo
-                          5. Módulo de reportes\n""")
+                          5. Módulo de reportes\n"""))
                     
+                    if respuestaUsuario == 1:
+                        
+                        system("cls")
+
+                        print("------------------------------------------------")   
+                        print("                INGRESAR NOTAS                  ")
+                        print("------------------------------------------------\n")   
+                        print("")
+
+                        salon = input("""      ingresa el salón    
+
+                                        1. Sputnik
+                                        2. Apolo
+                                        3. Artemis
+                        ------------------------------------------------\n""")
+
+                        if salon== "Sputnik":
+                            
+                            print("hi")
+                            print([Sputnik["estudiantes"]["nombres"]])
+
+                            IngresarEstudiante = input("Seleccione el estudiante:\n")
+                        
+                            for i in miJSON:
+                                for x in i["salon"]:
+                                    if x ["nombres"]== IngresarEstudiante:
+                                        print(x["nombres"])
+                            
+                            
+
+
                     
 
 
